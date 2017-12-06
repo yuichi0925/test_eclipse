1036fcebae0693aa272eedd9812573cd9b7e5ae7
祐一 <@home-PC>
2017-12-06 14:33:16 +0900

    あ

--- src/test_eclipse/Test_eclipse.java
+++ src/test_eclipse/Test_eclipse.java
@@ -4,11 +4,8 @@ public class Test_eclipse {
 
 	public static void main(String[] args) {
 		System.out.println("こんにちは");
-<<<<<<< HEAD
 		System.out.println("こんばんわ");
-=======
 		System.out.println("おはよう");
->>>>>>> branch 'master' of https://github.com/yuichi0925/test_eclipse.git
 	}
 
 }
